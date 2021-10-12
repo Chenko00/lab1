1478101bd19f2538f022821d1610fd630891851a
plitchenko_a <plitchenko_a@400D-58>
2021-10-12 11:00:45 +0700

    Работа над interface

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -2,11 +2,29 @@
 
 namespace ConsoleApp2
 {
+    public interface IMovable
+    {
+        const int minSpeed = 0;
+        private static int maxSpeed = 60;
+        static double GetTime(double distance, double speed) => distance / speed;
+        static int MaxSpeed
+        {
+            get { return maxSpeed; }
+            set
+            {
+                if (value > 0) maxSpeed = value;
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Lab 1");
+            Console.WriteLine(IMovable.MaxSpeed);
+            IMovable.MaxSpeed = 65;
+            Console.WriteLine(IMovable.MaxSpeed);
+            double time = IMovable.GetTime(100, 10);
+            Console.WriteLine(time);
         }
     }
 }
